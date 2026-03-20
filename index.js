767614d32b9700f6f63b493f116a4fe8033a8d2e
jaybyabut <jaybyabut@gmail.com>
2026-03-21 01:40:50 +0800

    merge

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import assesmentRouter from './routes/assessment.routes.js';
-<<<<<<< HEAD
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-=======
 import accountRouter from './routes/account.routes.js';
->>>>>>> 4e701612223cdfb9c339e90f66bdf0c034be9043
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,16 +16,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
 app.get("/assessment", (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend-test', 'assessment.html'));
 });
-=======
 app.use("/api/v1", assesmentRouter);
 app.use('/api/v1', accountRouter);
->>>>>>> 4e701612223cdfb9c339e90f66bdf0c034be9043
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server listening to port ${PORT}`);
 })
